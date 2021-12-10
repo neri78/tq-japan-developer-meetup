6656bd59bf2eb2791d110bb091ae0b1829a2eda0
neri78 <dikehara@gmail.com>
2021-12-10 14:40:00 +0900

    fixed value

--- levels/2021-q4-meetup-special-mission/objectives/twilio_phone_calls/validator.js
+++ levels/2021-q4-meetup-special-mission/objectives/twilio_phone_calls/validator.js
@@ -5,12 +5,12 @@ module.exports = async function (helper) {
   // Next, you test the user input - fail fast if they get one of the
   // answers wrong, or some aspect is wrong! Don't provide too much
   // negative feedback at once, have the player iterate.
-  if (!answer1 || !Number(answer1) || Number(answer1) != 25) {
+  if (!answer1 || !Number(answer1) || Number(answer1) != 250) {
     return helper.fail(`
       不正解です。Jeffの基調講演を視聴し、正しい値を丹生路y区してください。
     `);
   }
   helper.success(`
-    正解です！今年は既に25億コールを捌いています。
+    正解です！今年は既に250億コールを捌いています。
   `);
 };
